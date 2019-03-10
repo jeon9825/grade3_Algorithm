9d1075801cd780f294b0b0c21e07791c745e0447
jeon9825 <jeon9825@naver.com>
2019-03-10 23:21:16 +0900

    Add 01-Example2.java

---
+++ 01 알고리즘 설계와 분석의 기초/Example2.java
@@ -0,0 +1,12 @@
+public class Example2 {
+	static int sum(int n) {
+		if (n == 1)
+			return 1;
+		return n + sum(n - 1);
+	}
+
+	public static void main(String[] args) {
+		for (int i = 3; i <= 10; ++i)
+			System.out.printf("%d %d\n", i, sum(i));
+	}
+}
